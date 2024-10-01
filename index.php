ae9a30a0c925c8904c76c0c0b4c2ff77f4bdb92c
andromeda <androjagad2511@gmail.com>
2024-10-01 12:03:36 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote lokal to github</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quasi eveniet eum perferendis dolor quae at sint inventore labore delectus repudiandae consequuntur, quam hic nemo accusantium eos cumque sunt obcaecati.</h1>
+    
+</body>
+</html>
